237be21a2ae52d2b23ac3b73b7d3a5a08965b4d8
Bardia Keyvani <bardia.keyvani@spoton.com>
2025-03-17 20:25:37 -0700

    fix tests

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -4,7 +4,7 @@ import BookingForm from './BookingForm';
 
 const BookingPage = () => {
   return (
-    <section className="booking-page">
+    <section className="booking-page" role="region" aria-label="booking">
       <div className="booking-header">
         <h1>Reserve Your Table</h1>
       </div>
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock TextEncoder/TextDecoder for Jest tests
+// These are required by react-router-dom but not available in the Jest environment
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = require('util').TextEncoder;
+}
+
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = require('util').TextDecoder;
+}
